a96f55a993363ccbb863a89c89bb8cab8e49a951
Fabiola Villanueva <22103326@usc.edu.ph>
2025-10-10 20:32:32 +0800

    added draft file

---
+++ 013_CISCO Tutorials/001_computerWord.c
@@ -0,0 +1,21 @@
+//! Author: 		  Fabiola C. Villanueva
+//! Description:	Computer Word Practice Review for Tutorial
+//! Date: 	      10/10/2025
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure definition
+typedef unsigned char SET_0;
+typedef signed char SET_1;
+
+// function prototype
+// void displayBits (SET_0 set);
+// void displayBits (SET_1 set);
+
+int main () {
+  system("cls");
+  
+
+  return 0;
+}
